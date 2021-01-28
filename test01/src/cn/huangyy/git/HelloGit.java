3728567c89b4b6469ae75208598551a9b650390d
tom_glb <tom_glb@qq.com>
2021-01-28 14:51:03 +0800

    first commit

---
+++ test01/src/cn/huangyy/git/HelloGit.java
@@ -0,0 +1,9 @@
+package cn.huangyy.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+    }
+}
